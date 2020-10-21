682d7f16cf1607d3a075ccb4e209c25200ffca9f
renehorschinger <r.hoerschinger@gmx.at>
2020-10-21 17:48:16 +0200

    started with 3rd excercise

--- ue01/main.py
+++ ue01/main.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-import util as u
+from util import draw_line
 
 def canny():
     img = cv2.imread("data/porsche.png", cv2.IMREAD_GRAYSCALE)
@@ -9,8 +9,8 @@ def canny():
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-def hough():
-    img = cv2.imread("data/triangle.png", cv2.IMREAD_GRAYSCALE)
+def hough(image, threshold, same_line_thresh):
+    img = cv2.imread(image, cv2.IMREAD_GRAYSCALE)
     canny = cv2.Canny(img, 100, 200)
 
     step_d = 1
@@ -34,18 +34,66 @@ def hough():
 
     H = H / H.max()
 
-    threshold = 0.5
+    matrix_d_a = []
     for (d, angle) in np.argwhere(H >= threshold):
+        buff = []
         if d > max_d:
             d = d - range_d
-        print(d, " ", angle)
-        u.draw_line(img, d, np.deg2rad(angle))
+
+        buff.append(d)
+        buff.append(angle)
+        matrix_d_a.append(buff)
+
+    print(matrix_d_a)
+
+    if len(matrix_d_a) > 1:
+        index = 0
+
+        same_line_d_a = []
+        while index in range(0, len(matrix_d_a)):
+            similar = False
+            if index != len(matrix_d_a) - 1:
+                if matrix_d_a[index + 1][0] in range(matrix_d_a[index][0] - same_line_thresh, matrix_d_a[index][0] + same_line_thresh):
+                    if matrix_d_a[index + 1][1] in range(matrix_d_a[index][1] - same_line_thresh, matrix_d_a[index][1] + same_line_thresh):
+                        similar = True
+
+            buff = []
+            buff.append(matrix_d_a[index][0])
+            buff.append(matrix_d_a[index][1])
+            same_line_d_a.append(buff)
+
+            if not similar:
+                print("\nSame lines: ", same_line_d_a)
+
+                # now print the line with the highes vote
+                max = 0
+                current_max = 0
+
+                for i in range(0, len(same_line_d_a)):
+                    h = H[same_line_d_a[i][0], same_line_d_a[i][1]]
+                    if h > current_max:
+                        current_max = h
+                        max = same_line_d_a[i]
+
+                print("Highes Vote: ", H[max[0], max[1]], " ", max)
+                draw_line(img, max[0], np.deg2rad(max[1]))
+
+                same_line_d_a = []
+
+            index += 1
+
+    else:
+        draw_line(img, d, np.deg2rad(angle))
 
     cv2.imshow("test", img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
+    return H
+
 if __name__ == '__main__':
-    hough()
+    hough("data/triangle.png", 0.4, 5)
+
+
 
 
