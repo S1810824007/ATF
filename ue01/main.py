99175d541db527ccfdf7f8a15732e70d0c479f8b
renehorschinger <r.hoerschinger@gmx.at>
2020-10-22 18:50:23 +0200

    some adjustments

--- ue01/main.py
+++ ue01/main.py
@@ -9,9 +9,10 @@ def canny():
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-def hough(image, threshold, same_line_thresh):
-    img = cv2.imread(image, cv2.IMREAD_GRAYSCALE)
-    canny = cv2.Canny(img, 100, 200)
+def hough(image, threshold, same_line_thresh, STEPS=False):
+    img = cv2.imread(image)
+    canny = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    canny = cv2.Canny(canny, 100, 200)
 
     step_d = 1
     step_angle = 1
@@ -36,40 +37,42 @@ def hough(image, threshold, same_line_thresh):
 
     matrix_d_a = []
     for (d, angle) in np.argwhere(H >= threshold):
-        buff = []
         if d > max_d:
             d = d - range_d
 
-        buff.append(d)
-        buff.append(angle)
-        matrix_d_a.append(buff)
+        matrix_d_a.append([d,angle])
 
-    print(matrix_d_a)
+    print("\nAll lines: ", matrix_d_a)
 
     if len(matrix_d_a) > 1:
         index = 0
 
-        same_line_d_a = []
+        already_used = []
         while index in range(0, len(matrix_d_a)):
-            similar = False
-            if index != len(matrix_d_a) - 1:
-                if matrix_d_a[index + 1][0] in range(matrix_d_a[index][0] - same_line_thresh, matrix_d_a[index][0] + same_line_thresh):
-                    if matrix_d_a[index + 1][1] in range(matrix_d_a[index][1] - same_line_thresh, matrix_d_a[index][1] + same_line_thresh):
-                        similar = True
+            same_line_d_a = []
+            current = matrix_d_a[index]
+            if current not in already_used:
+                same_line_d_a.append(current)
+                already_used.append(current)
+                j = index + 1
 
-            buff = []
-            buff.append(matrix_d_a[index][0])
-            buff.append(matrix_d_a[index][1])
-            same_line_d_a.append(buff)
+                while j < len(matrix_d_a):
+                    if matrix_d_a[j] not in already_used:
+                        if matrix_d_a[j][0] in range(current[0] - same_line_thresh, current[0] + same_line_thresh):
+                            if matrix_d_a[j][1] in range(current[1] - same_line_thresh, current[1] + same_line_thresh):
+                                # identical lines and not used already -> add them to same_line_d_a
+                                buff = matrix_d_a[j]
+                                already_used.append(buff)
+                                same_line_d_a.append(buff)
+                    j += 1
 
-            if not similar:
+            # now print the line with the highes vote
+            max = 0
+            current_max = 0
+            if same_line_d_a != []:
                 print("\nSame lines: ", same_line_d_a)
-
-                # now print the line with the highes vote
-                max = 0
-                current_max = 0
-
                 for i in range(0, len(same_line_d_a)):
+                    #draw_line(img, same_line_d_a[i][0], np.deg2rad(same_line_d_a[i][1]), (255,0,0))
                     h = H[same_line_d_a[i][0], same_line_d_a[i][1]]
                     if h > current_max:
                         current_max = h
@@ -78,21 +81,25 @@ def hough(image, threshold, same_line_thresh):
                 print("Highes Vote: ", H[max[0], max[1]], " ", max)
                 draw_line(img, max[0], np.deg2rad(max[1]))
 
-                same_line_d_a = []
+                if STEPS:
+                    cv2.imshow(image, img)
+                    cv2.waitKey(0)
+                    cv2.destroyAllWindows()
 
             index += 1
 
     else:
         draw_line(img, d, np.deg2rad(angle))
 
-    cv2.imshow("test", img)
+    cv2.imshow(image, img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
     return H
 
 if __name__ == '__main__':
-    hough("data/triangle.png", 0.4, 5)
+    hough("data/triangle.png", 0.3, 10, True)
+    #hough("data/dice.png", 0.5, 25, True)
 
 
 
